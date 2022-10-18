611a719530c3a62acf3d4bdc362ca8a692bdea14
LDJ <957058831@qq.com>
2022-10-18 22:16:59 +0800

    1.delete

--- thread_pool/threadPool.hpp
+++ thread_pool/threadPool.hpp
@@ -36,11 +36,11 @@ private:
 
 template<typename T>
 void ThreadPool<T>::run() {
-    while (!stop) { //  第一次没有写 while 循环 如果不写while循环 线程就不能重复利用
+    while (!stop) {
         shared_ptr<T> re;
         {
             AutoLock lc(mutex_);
-            continue_thread.wait(lc, [this]() {return !this->taskQueue.empty(); }); //条件不满足线程挂起，并解锁
+            continue_thread.wait(lc, [this]() {return !this->taskQueue.empty(); });
             if (this->taskQueue.empty()) {
                 return;
             }
@@ -48,7 +48,7 @@ void ThreadPool<T>::run() {
             this->taskQueue.pop();
            // cout << "task size" << this->taskQueue.size() << endl;
             if (re != nullptr) {
-                re->process();  // 任务处理
+                re->process();
             }
         }
     }
@@ -71,13 +71,13 @@ void ThreadPool<T>::appendTask(shared_ptr<T> task) {
 template<typename T>
 ThreadPool<T>::~ThreadPool() {
     stop = true;
-    continue_thread.notify_all();// 唤醒所有阻塞线程
+    continue_thread.notify_all();
     for (auto &iter:thread_container){
         if (iter.joinable()) {
-            iter.join();// 同步线程释放资源
+            iter.join();
         }
     }
-  // 需要清理 vector吗？
+
 }
 
 template<typename T>
@@ -87,7 +87,7 @@ ThreadPool<T>::ThreadPool(int num):currentThreadNum(num),stop(false)
             throw exception();
     }
     for (auto i = 0; i<currentThreadNum; ++i ){
-       thread_container.emplace_back(ThreadPool::worker,this);//封装worker线程  thread_container.emplace_back((ThreadPool::worker,this))  这样写是错误的
+       thread_container.emplace_back(ThreadPool::worker,this);
     }
 };
 
--- thread_pool/源.cpp
+++ thread_pool/源.cpp
@@ -9,18 +9,17 @@ std::mutex mtx;           // mutex for critical section
 void print_block(int n, char c) {
 	// critical section (exclusive access to std::cout signaled by locking mtx):
 	
-	std::cout << "test 3s "<< std::this_thread::get_id() << "\n";//线程1 2 同时执行
-	mtx.lock();//线程2 阻塞3s
-	//for (int i = 0; i < n; ++i) { std::cout << c; }
-	//std::cout << '\n';
-	std::this_thread::sleep_for(std::chrono::milliseconds(1000 * 3));//两者线程互斥
+	std::cout << "test 3s "<< std::this_thread::get_id() << "\n";
+	mtx.lock();
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(1000 * 3));
 	std::cout << "wait for 3 seconds \n";
-	mtx.unlock();//如果不解锁 线程就会一直互斥 即不会有线程执行该函数。或者说第二个线程会一直等待解锁
-	std::cout << "test 6s\n" << std::this_thread::get_id() << "\n";//不会执行同时？
+	mtx.unlock();
+	std::cout << "test 6s\n" << std::this_thread::get_id() << "\n";
     std::this_thread::sleep_for(std::chrono::milliseconds(1000 * 6));
-    std::cout << "wait for 6 seconds"<< std::this_thread::get_id() << "\n" ;//前面线程比后面线程 快3s 前是9s 执行完成 后是12s执行完成 
+    std::cout << "wait for 6 seconds"<< std::this_thread::get_id() << "\n" ;
     std::cout << '\n';
-	//sum :即 阻塞线程不会执行后面没有lock 的对象，而是要等待锁线程unlock 释放锁时才会执行后面得代码
+
 
 }
 
